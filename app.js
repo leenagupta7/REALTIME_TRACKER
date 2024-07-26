aa9d46048f72fc78ca2583dd525f5a3dcd405647
bemybunny <bemybunny123@gmail.com>
2024-07-26 19:00:59 +0530

    first commit

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function(socket){
+    socket.on('send-location',function(data){
+        io.emit('receive-location',{id:socket.id,...data});
+    })
+    socket.on('disconnect',function(){
+        io.emit('user-disconnected',socket.id);
+    })
 console.log("connected");
 });
 
